c2ca7ddef9bae7e3ace13085bb8c0186f305f235
hopark <hopark@student.42.fr>
2020-10-01 17:51:10 +0900

    3

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 12:24:59 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/01 17:02:54 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/01 17:50:59 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ void		*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	ptr2 = (unsigned char *)src;
 	while (n-- > 0)
 	{
-		*(ptr1++) = *(ptr2++);
 		if (*ptr2 == (unsigned char)c)
+		{
+			*(ptr1++) = *(ptr2++);
 			return (ptr1);
+		}
 	}
 	return (0);
 }
