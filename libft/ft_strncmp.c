6a39eae4341cc3a4bc283ae8c5279fc0fc2c2214
hopark <hopark@student.42.fr>
2020-10-02 11:29:07 +0900

    5

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:11:59 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/02 11:17:34 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/02 11:28:55 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	c1 = (unsigned char *)s1;
 	c2 = (unsigned char *)s2;
 	i = 0;
+	if (n <= 0)
+		return (0);
 	while (i < n && c1[i] == c2[i])
 		i++;
 	return (c1[i] - c2[i]);
