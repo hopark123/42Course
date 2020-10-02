7b240c61688ce60eb3a2a5ddb8143989e386043a
hopark <hopark@student.42.fr>
2020-10-02 11:07:46 +0900

    4

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:11:55 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/02 11:06:04 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/02 11:07:38 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 char		*ft_strchr(const char *s, int c)
 {
-	size_t	i;
+	size_t				i;
 	unsigned char		c2;
+	unsigned char		*s2;
 
 	c2 = c;
+	s2 = (unsigned char *)s;
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c2)
-			return ((char *)s + i);
+		if (s2[i] == c2)
+			return ((char *)s2 + i);
 		i++;
 	}
 	return (0);
