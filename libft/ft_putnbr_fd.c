d6a5a25715c2516ae3108b16f49cd60afa902a2f
hopark <hopark@student.42.fr>
2020-10-02 12:05:17 +0900

    3

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 17:04:40 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/02 12:03:35 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/02 12:04:59 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 		n2 = (unsigned int)n * (-1);
 	}
-	n2 = (unsigned int)n;
+	else
+		n2 = (unsigned int)n;
 	if (n2 >= 10)
 		ft_putnbr_fd((int)(n2 / 10), fd);
 	ft_putchar_fd((int)(n2 % 10 + '0'), fd);
