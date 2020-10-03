dd360509cf01f36cbca3448f8ba88e3984cf289d
hopark <hopark@student.42.fr>
2020-10-03 19:41:12 +0900

    6

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 18:26:45 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/03 18:30:36 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/03 19:40:16 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_lstadd_front(t_list **list, t_list *new)
+void		ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (lst == 0 || new == 0)
 		return ;
