21ed443242d657a1b16df8818348d400b78285c6
hopark <hopark@student.42.fr>
2020-10-03 19:15:04 +0900

    1

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 18:55:36 by hopark            #+#    #+#             */
+/*   Updated: 2020/10/03 19:01:31 by hopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_lst **lst, void (*del)(void *))
+{
+	t_list *temp;
+
+	if (lst == 0 || del == 0)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(lst, del);
+		*lst = temp;
+	}
+	*lst = 0;
+}
