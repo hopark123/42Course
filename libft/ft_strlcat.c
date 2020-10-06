9c77b03932a1ff0d68b0e454d93ad73ed5f9037a
hopark <hopark@student.42.fr>
2020-10-06 13:46:12 +0900

    1

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:11:56 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/06 13:42:53 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/06 13:45:49 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	i = 0;
-	if (dst_len < size)
-		return (dst_len + src_len);
-	while (src[i] && dst_len + i + 1 < size)
+	while (!src[i] && dst_len + i + 1 < size)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = 0;
+	if (dst_len < size)
+		return (dst_len + src_len);
 	return (size + src_len);
 }
