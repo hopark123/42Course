3abafca0bc6a6af8c324bbdb89246af0221b0ace
Hojun Park <hopark@c5r7s7.42seoul.kr>
2020-10-09 00:03:36 +0900

    1

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hopark <hopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 14:52:52 by hopark            #+#    #+#             */
-/*   Updated: 2020/10/08 23:40:05 by hopark           ###   ########.fr       */
+/*   Updated: 2020/10/09 00:03:06 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	if (len1 == 0 && len2 == 0)
-		return (ft_strdup(""));
+	if (!s1 && !s2)
+		return (0);
 	if (!(result = (char *)(malloc(sizeof(char) * (len1 + len2 + 1)))))
 		return (0);
 	ft_memcpy(result, s1, len1);
