bf26d1201d5788dcf8b76ad735ff9a26a9e5ccc3
Hojun Park <hopark@c1r3s6.42seoul.kr>
2021-01-23 15:53:45 +0900

    210123

--- 02_cub3d/mlx_example/00ex/main.c
+++ 02_cub3d/mlx_example/00ex/main.c
@@ -23,14 +23,16 @@
 # define KEY_S			1
 # define KEY_D			2
 
+
+
 int minimap = 5;
 int TILESIZE = 32;
 int mapx = 8, mapy = 8;
 int map[] = {
 	1,1,1,1,1,1,1,1,
 	1,0,0,0,0,0,0,1,
-	1,0,0,0,0,0,0,1,
 	1,0,0,0,1,0,0,1,
+	1,0,1,1,1,1,0,1,
 	1,0,0,0,1,0,0,1,
 	1,0,0,0,0,0,0,1,
 	1,0,0,0,0,0,0,1,
@@ -64,15 +66,20 @@ typedef struct	s_game
 	void		*win;
 	t_img		img;
 	t_fvector	position;
-	float		angle;
 	t_fvector	dirction;
 	t_fvector	turn;
 	t_fvector	move;
 	t_fvector	side;
 	t_ivector	map;
+	t_fvector	wall_h;
+	t_fvector	wall_v;
+	t_ivector	hit;
+	float		angle;
+
 }				t_game;
 
 
+int		check_wall(t_game *game, t_fvector pt);
 
 int				key_press(int keycode, t_game *game)
 {
@@ -131,6 +138,18 @@ void		map_check(t_game *game, char *map)
 	game->map.y = 8;
 }
 
+void	darw_line(t_game *game, t_fvector pt1, t_fvector pt2)
+{
+	// float y_step = (pt2.y - pt1.y) / (pt2.x - pt1.x);
+	// for(int i = 0; i < abs(pt2.x-pt1.x); i++)
+	// 	game->img.data[(int)(pt1.y + i) * IMG_WIDTH + (int)(pt1.x + i)] = 0xFFFF00;
+	float angle = (pt2.y-pt1.y) / (pt2.x - pt1.x);
+	for(int i = 0; i < 50; i++)
+		{
+			game->img.data[(int)(pt1.y + i*cos(angle)) * IMG_WIDTH + (int)(pt1.x + i*sin(angle))] = 0xFFFF00;
+		}
+}
+
 void		draw_square(t_game *game, t_ivector pt, int size)
 {
 	int 	i;
@@ -147,6 +166,104 @@ void		draw_square(t_game *game, t_ivector pt, int size)
 		}
 	i++;
 	}
+}
+
+void	hit_horztal(t_game *game)
+{
+	int		face_up;
+	int		face_right;
+	t_fvector		intercept;
+	t_fvector		step;
+	t_fvector		next;
+
+	face_up = 1;
+	if (game->angle > 0 && game->angle < M_PI)
+		face_up = -1;
+	face_right = -1;
+	if (game->angle < 0.5 * M_PI || game->angle > 1.5 * M_PI)
+		face_right = 1;
+	intercept.y = floor(game->position.y / TILESIZE) * TILESIZE;
+	intercept.y += (face_up == 1) ? 0 : TILESIZE;
+	intercept.x = game->position.x + (intercept.y - game->position.y) / tan(game->angle);
+
+	step.y = TILESIZE;
+	step.y *= (face_up  == 1) ? -1 : 1;
+
+	step.x = TILESIZE / tan(game->angle);
+	step.x *= (face_right == -1 && step.x > 0) ? -1 : 1;
+	step.x *= (face_right == 1 && step.x < 0) ? -1 : 1;
+
+	next.y = (face_up == 1) ? intercept.y - 1 : intercept.y;
+	next.x = intercept.x;
+	// printf("angle : %f   tan : %f\n", game->angle, tan(game->angle));
+	// printf("angle : %f right : %d up  :%d \n",  game->angle, face_right, face_up);
+	// printf("step x : %f y : %f\n\n", step.x, step.y);
+
+	while (next.x >= 0 && next.x <= WIN_WIDTH && next.y >= 0 && next.y <= WIN_HEIGHT)
+		if (!check_wall(game, next))
+		{
+			game->hit.y = 1;
+			game->wall_h.y = next.y;
+			game->wall_h.x = next.x;
+			// printf("wall_h x : %f y : %f\n",game->wall_h.x, game->wall_h.y);
+			game->img.data[(int)(game->wall_h.y) * IMG_WIDTH + (int)(game->wall_h.x)] = 0xFF000F;
+
+			// darw_line(game, game->position, game->wall_h);
+			break;
+		}
+		else{
+			next.y += step.y;
+			next.x += step.x;
+		}
+}
+
+void	hit_vertical(t_game *game)
+{
+	int		face_up;
+	int		face_right;
+	t_fvector		intercept;
+	t_fvector		step;
+	t_fvector		next;
+
+	face_up = 1;
+	if (game->angle > 0 && game->angle < M_PI)
+		face_up = -1;
+	face_right = -1;
+	if (game->angle < 0.5 * M_PI || game->angle > 1.5 * M_PI)
+		face_right = 1;
+	intercept.y = floor(game->position.y / TILESIZE) * TILESIZE;
+	intercept.y += (face_up == 1) ? 0 : TILESIZE;
+	intercept.x = game->position.x + (intercept.y - game->position.y) / tan(game->angle);
+
+	step.y = TILESIZE;
+	step.y *= (face_up  == 1) ? -1 : 1;
+
+	step.x = TILESIZE / tan(game->angle);
+	step.x *= (face_right == -1 && step.x > 0) ? -1 : 1;
+	step.x *= (face_right == 1 && step.x < 0) ? -1 : 1;
+
+	next.y = (face_up == 1) ? intercept.y - 1 : intercept.y;
+	next.x = intercept.x;
+	// printf("angle : %f   tan : %f\n", game->angle, tan(game->angle));
+	// printf("angle : %f right : %d up  :%d \n",  game->angle, face_right, face_up);
+	// printf("step x : %f y : %f\n\n", step.x, step.y);
+
+	while (next.x >= 0 && next.x <= WIN_WIDTH && next.y >= 0 && next.y <= WIN_HEIGHT)
+		if (!check_wall(game, next))
+		{
+			game->hit.x = 1;
+			game->wall_v.y = next.y;
+			game->wall_v.x = next.x;
+			// printf("wall_v x : %f y : %f\n",game->wall_v.x, game->wall_v.y);
+			game->img.data[(int)(game->wall_v.y) * IMG_WIDTH + (int)(game->wall_v.x)] = 0xFF000F;
+
+			// darw_line(game, game->position, game->wall_v);
+			break;
+		}
+		else{
+			next.y += step.y;
+			next.x += step.x;
+		}
 }
 
 void		map_draw(t_game *game)
@@ -193,26 +310,28 @@ void		img_init(t_game *game)
 
 }
 
-
 int		update_image(t_game *game)
 {
 	for(int j = 0; j < 1; j++)
 	{
 		for(int i = 0; i < 50; i++)
 		{
-			game->img.data[(int)(game->position.y + i*cos(game->angle + 0.01 * j)) * IMG_WIDTH + (int)(game->position.x + i*sin(game->angle + 0.01 *j))] = 0xFF0F00 + j;
+			game->img.data[(int)(game->position.y + i*sin(game->angle + 0.01 * j)) * IMG_WIDTH + (int)(game->position.x + i*cos(game->angle + 0.01 *j))] = 0xFF0F00 + j;
 		}
 	}
+	hit_horztal(game);
 	for(int i = -5; i <5; i++)
 		for(int j = -5; j < 5; j++)
 			game->img.data[((int)game->position.y + i) * IMG_WIDTH + ((int)game->position.x + j)] = 0xFF00FF;
 	mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
 	// printf("x : %f y : %f\n",  game->position.x, game->position.y);
 	// printf("dx : %f dy : %f\n",  game->dirction.x, game->dirction.y);
+	// printf("angle : %f\n",  game->angle);
+
 	return(0);
 }
 
-int		check_wall(t_game *game, t_fvector pt, int a)
+int		check_wall(t_game *game, t_fvector pt)
 {
 	int		mapx;
 	int		mpay;
@@ -231,7 +350,7 @@ int		player_move(t_game *game)
 		t_fvector next_pt;
 		next_pt.x = game->position.x + game->dirction.x;
 		next_pt.y = game->position.y + game->dirction.y;
-		if (check_wall(game, next_pt, 1))
+		if (check_wall(game, next_pt))
 			game->position = next_pt;
 	}
 	else if (game->move.y == 1)
@@ -239,7 +358,7 @@ int		player_move(t_game *game)
 		t_fvector next_pt;
 		next_pt.x = game->position.x - game->dirction.x;
 		next_pt.y = game->position.y - game->dirction.y;
-		if (check_wall(game, next_pt, 1))
+		if (check_wall(game, next_pt))
 			game->position = next_pt;
 	}
 	return(1);
@@ -250,15 +369,15 @@ int		player_turn(t_game *game)
 	int		speed;
 	speed = 2;
 	if (game->turn.x == 1)
-		game->angle -= M_PI/180;
-	else if (game->turn.y == 1)
 		game->angle += M_PI/180;
+	else if (game->turn.y == 1)
+		game->angle -= M_PI/180;
 	if (game->angle < 0)
 		game->angle += 2*M_PI;
 	if (game->angle > 2*M_PI)
 		game->angle -= 2*M_PI;
-	game->dirction.x = speed * sin(game->angle);
-	game->dirction.y = speed * cos(game->angle);
+	game->dirction.x = speed * cos(game->angle);
+	game->dirction.y = speed * sin(game->angle);
 	return(1);
 }
 
@@ -283,7 +402,8 @@ int		main(void)
 	t_game		game;
 	game.position.x = 100;
 	game.position.y = 150;
-	game.angle = M_PI;
+	game.angle = 0;
+	
 	window_init(&game);
 	img_init(&game);
 	mlx_hook(game.win, X_EVENT_KEY_PRESS, 0, &key_press, &game);
