950ce4c2f0d34bbce35c517a8f083402183f64c9
hjpark <hjpark@hjparkui-MacBookAir.local>
2021-02-09 13:30:57 +0900

    02091330

--- 02_cub3d/mlx_example/00ex/main copy.c
+++ 02_cub3d/mlx_example/00ex/main copy.c
@@ -4,65 +4,67 @@
 #include <math.h>
 #define _USE_MATH_DEFINES
 
-# define WIN_WIDTH 800
-# define WIN_HEIGHT 600
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
 
-# define IMG_WIDTH 800
-# define IMG_HEIGHT 600
+#define IMG_WIDTH 800
+#define IMG_HEIGHT 600
 
-#define X_EVENT_KEY_PRESS		2
-#define X_EVENT_KEY_RELEASE		3
-#define X_EVENT_KEY_EXIT		17 //exit key code
-#define KEY_ESC			53
-# define KEY_Q			12
-# define KEY_W			13
-# define KEY_E			14
-# define KEY_R			15
-# define KEY_A			0
-# define KEY_S			1
-# define KEY_D			2
+#define TEXTURE_SIZE 64
 
-
+#define X_EVENT_KEY_PRESS 2
+#define X_EVENT_KEY_RELEASE 3
+#define X_EVENT_KEY_EXIT 17 //exit key code
+#define KEY_ESC 53
+#define KEY_Q 12
+#define KEY_W 13
+#define KEY_E 14
+#define KEY_R 15
+#define KEY_A 0
+#define KEY_S 1
+#define KEY_D 2
 
 int minimap = 5;
 int TILESIZE = 15;
 int mapx = 16, mapy = 8;
-float	FOV_ANGLE = 60 * (M_PI / 180);
-int	WALL_STRIP_WIDTH = 1;
+float FOV_ANGLE = 60 * (M_PI / 180);
+int WALL_STRIP_WIDTH = 1;
 // int	NUM_RAYS = WIN_WIDTH / WALL_STRIP_WIDTH;
-int	NUM_RAYS = 800;
+int NUM_RAYS = 800;
 
 int map[] = {
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,
-	1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,1,
-	1,0,0,0,1,0,0,1,1,0,1,0,0,0,0,1,
-	1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,
-	1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 };
 
 typedef struct s_fvector
 {
-	float		x;
-	float		y;
-}				t_fvector;
+	float x;
+	float y;
+} t_fvector;
 
 typedef struct s_ivector
 {
-	int			x;
-	int			y;
-}				t_ivector;
+	int x;
+	int y;
+} t_ivector;
 
-typedef struct	s_img
+typedef struct s_img
 {
-	void		*img;
-	int			*data;
-	int			size_l;
-	int			bpp;
-	int			endian;
-}				t_img;
+	void *ptr;
+	int *data;
+	int width;
+	int height;
+	int size_l;
+	int bpp;
+	int endian;
+} t_img;
 
 typedef struct s_ray
 {
@@ -75,51 +77,62 @@ typedef struct s_ray
 	t_ivector	step;
 	t_ivector	map_pos;
 	int			check;
+	float		dis;
 	float		len;
 	char		dir;
-}				t_ray;
-
-typedef struct	s_game
+	float		point;
+}			t_ray;
+typedef struct s_draw
 {
-	void		*mlx;
-	void		*win;
-	t_img		img;
-	t_fvector	pos;
-	t_ivector	map_pos;
-	t_ivector	map_size;
-	t_fvector	dir;
-	t_ray		ray;
-	t_fvector	plane;
-	t_fvector	turn;
-	t_fvector	move;
+	t_fvector	put;
+	t_fvector	tex;
+	float		start;
+	float		end;
+	float		step;
+	float		pixel;
+	unsigned int	color;
+}				t_draw;
 
-	float		angle;
+typedef struct s_game
+{
+	void *mlx;
+	void *win;
+	t_img img;
+	t_img tex[7];
+	t_fvector pos;
+	t_ivector map_size;
+	t_fvector dir;
+	t_ray ray;
+	t_fvector plane;
+	t_fvector turn;
+	t_fvector move;
 
-}				t_game;
+	float angle;
 
+} t_game;
 
-int		check_fwall(t_game *game, t_fvector pt);
+int check_fwall(t_game *game, t_fvector pt);
 
-t_fvector	roation(t_fvector src, float angle)
+t_fvector roation(t_fvector src, float angle)
 {
 	t_fvector dest;
-	
+
 	dest.x = cos(angle) * src.x - sin(angle) * src.y;
 	dest.y = sin(angle) * src.x + cos(angle) * src.y;
 	return (dest);
 }
 
-float		ft_max(float a, float b)
+float ft_max(float a, float b)
 {
 	return (a > b ? a : b);
 }
 
-float		ft_min(float a, float b)
+float ft_min(float a, float b)
 {
 	return (a < b ? a : b);
 }
 
-int			key_press(int keycode, t_game *game)
+int key_press(int keycode, t_game *game)
 {
 	if (keycode == KEY_W)
 		game->move.x = 1;
@@ -132,7 +145,7 @@ int			key_press(int keycode, t_game *game)
 	return (0);
 }
 
-int			key_release(int keycode, t_game *game)
+int key_release(int keycode, t_game *game)
 {
 	if (keycode == KEY_W)
 		game->move.x = 0;
@@ -145,7 +158,7 @@ int			key_release(int keycode, t_game *game)
 	return (0);
 }
 
-float		vector_len(t_fvector v1, t_fvector v2)
+float vector_len(t_fvector v1, t_fvector v2)
 {
 	float tempx;
 	float tempy;
@@ -161,28 +174,28 @@ float		vector_len(t_fvector v1, t_fvector v2)
 	return (tempx);
 }
 
-void		window_init(t_game *game)
+void window_init(t_game *game)
 {
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, "mlx 42");
 }
 
-void		map_check(t_game *game, char *map)
+void map_check(t_game *game, char *map)
 {
 	game->map_size.x = 16;
 	game->map_size.y = 8;
 }
 
-void		draw_pixel(t_game *game, t_fvector pt, unsigned int color)
+void draw_pixel(t_game *game, t_fvector pt, unsigned int color)
 {
 	game->img.data[(int)pt.y * IMG_WIDTH + (int)pt.x] = color;
 }
 
-void		draw_line(t_game *game, t_fvector v1, t_fvector v2, int color)
+void draw_line(t_game *game, t_fvector v1, t_fvector v2, int color)
 {
-	float	dx;
-	float	dy;
-	float	step;
+	float dx;
+	float dy;
+	float step;
 
 	dx = (v2.x - v1.x);
 	dy = (v2.y - v1.y);
@@ -197,58 +210,58 @@ void		draw_line(t_game *game, t_fvector v1, t_fvector v2, int color)
 	}
 }
 
-void		draw_square(t_game *game, t_ivector pt, int size, unsigned int color)
+void draw_square(t_game *game, t_ivector pt, int size, unsigned int color)
 {
-	int 	i;
-	int 	j;
+	int i;
+	int j;
 
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		j = 0;
-		while(j < size)
+		while (j < size)
 		{
-			game->img.data[(size * pt.y + i)*IMG_WIDTH  + size * pt.x + j] = color;
+			game->img.data[(size * pt.y + i) * IMG_WIDTH + size * pt.x + j] = color;
 			j++;
 		}
-	i++;
+		i++;
 	}
 }
 
-int			check_fwall(t_game *game, t_fvector pt)
+int check_fwall(t_game *game, t_fvector pt)
 {
 	if (pt.x < 0 || pt.x > game->map_size.x || pt.y < 0 || pt.y > game->map_size.y)
 		return (1);
 	return (map[(int)pt.y * game->map_size.x + (int)pt.x] != 0);
 }
 
-int			check_iwall(t_game *game, t_ivector pt)
+int check_iwall(t_game *game, t_ivector pt)
 {
 	if (pt.x < 0 || pt.x > game->map_size.x || pt.y < 0 || pt.y > game->map_size.y)
 		return (1);
 	return (map[pt.y * game->map_size.x + pt.x] != 0);
 }
 
-void		map_draw(t_game *game)
+void map_draw(t_game *game)
 {
-	int		i;
-	int		j;
+	int i;
+	int j;
 
 	i = 0;
 	while (i < game->map_size.y)
 	{
 		j = 0;
-		while(j < game->map_size.x)
+		while (j < game->map_size.x)
 		{
 			t_ivector pt;
-			
+
 			pt.x = j;
 			pt.y = i;
-			if(map[(i * (int)game->map_size.x + j)])
+			if (map[(i * (int)game->map_size.x + j)])
 				draw_square(game, pt, TILESIZE, 0x868e96);
 			else
 				draw_square(game, pt, TILESIZE, 0x000000);
-		j++;
+			j++;
 		}
 		i++;
 	}
@@ -258,22 +271,68 @@ void		map_draw(t_game *game)
 		j = 0;
 		while (j <= game->map_size.x * TILESIZE)
 		{
-			if(i % TILESIZE == 0 || j % TILESIZE == 0)
-				game->img.data[i *IMG_WIDTH + j] = 0xFFFFFF;
+			if (i % TILESIZE == 0 || j % TILESIZE == 0)
+				game->img.data[i * IMG_WIDTH + j] = 0xFFFFFF;
 			j++;
 		}
 		i++;
 	}
 }
 
-void		img_init(t_game *game)
+void	ft_swap(int *a,  int *b)
 {
-	game->img.img = mlx_new_image(game->mlx, IMG_WIDTH, IMG_HEIGHT);
-	game->img.data = (int *)mlx_get_data_addr(game->img.img, &game->img.bpp, &game->img.size_l, &game->img.endian);
+	int		temp;
+	temp = *a;
+	*a = *b;
+	*b = temp; 
+}
+
+void	tex_init(t_game *game, int num, char *path)
+{
+	game->tex[num].ptr = mlx_xpm_file_to_image(game->mlx, path, &game->tex[num].width, &game->tex[num].height);
+	game->tex[num].data = (int *)mlx_get_data_addr(game->tex[num].ptr, &game->tex[num].bpp, &game->tex[num].size_l, &game->tex[num].endian);
+	int i;
+	int j;
+	i = 0;
+	// while (i < TEXTURE_SIZE)
+	// {
+	// 	j = 0;
+	// 	while (j < i)
+	// 	{
+	// 		ft_swap(&game->tex[num].data[j * TEXTURE_SIZE + i], &game->tex[num].data[i * TEXTURE_SIZE + j]);
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
+}
+
+void img_init(t_game *game)
+{
+	int		i;
+	game->img.ptr = mlx_new_image(game->mlx, IMG_WIDTH, IMG_HEIGHT);
+	game->img.data = (int *)mlx_get_data_addr(game->img.ptr, &game->img.bpp, &game->img.size_l, &game->img.endian);
+	i = 0;
+	tex_init(game, 0, "img/wall_e.xpm");
+	tex_init(game, 1, "img/wall_w.xpm");
+	tex_init(game, 2, "img/wall_s.xpm");
+	tex_init(game, 3, "img/wall_n.xpm");
+
+				for (int i = 0; i < IMG_WIDTH; i++)
+					for (int j = 0; j < IMG_HEIGHT /2 ; j++)
+					{
+						t_fvector v = {i, j};
+						draw_pixel(game, v, 0XFFBBFF);
+					}
+				for (int i = 0; i < IMG_WIDTH; i++)
+					for (int j = IMG_HEIGHT/2; j < IMG_HEIGHT ; j++)
+					{
+						t_fvector v = {i, j};
+						draw_pixel(game, v, 0XFFAA00);
+					}
 	map_check(game, 0);
 }
 
-void		ray_quadrant(t_ray *ray)
+void ray_quadrant(t_ray *ray)
 {
 	if (ray->x >= 0 && ray->y < 0)
 		ray->q = 1;
@@ -285,13 +344,13 @@ void		ray_quadrant(t_ray *ray)
 		ray->q = 4;
 }
 
-void		ray_init(t_game *game, t_ray *ray, int i)
+void ray_init(t_game *game, t_ray *ray, int i)
 {
 	ray->check = 0;
 	ray->map_pos.x = (int)game->pos.x;
 	ray->map_pos.y = (int)game->pos.y;
 
-	ray->cam = 2 * i / (float) NUM_RAYS - 1;
+	ray->cam = 2 * i / (float)NUM_RAYS - 1;
 	ray->x = game->dir.x + game->plane.x * ray->cam;
 	ray->y = game->dir.y + game->plane.y * ray->cam;
 	ray_quadrant(ray);
@@ -299,7 +358,7 @@ void		ray_init(t_game *game, t_ray *ray, int i)
 	ray->delta.y = fabs(1 / ray->y);
 }
 
-void		ray_dda_init(t_game *game, t_ray *ray)
+void ray_dda_init(t_game *game, t_ray *ray)
 {
 	if (ray->q == 2 || ray->q == 3)
 	{
@@ -323,7 +382,7 @@ void		ray_dda_init(t_game *game, t_ray *ray)
 	}
 }
 
-void 		ray_dda(t_game *game, t_ray *ray)
+void ray_dda(t_game *game, t_ray *ray)
 {
 	while (ray->check == 0)
 	{
@@ -331,7 +390,7 @@ void 		ray_dda(t_game *game, t_ray *ray)
 		{
 			ray->side.x += ray->delta.x;
 			ray->map_pos.x += ray->step.x;
-			if (ray->q == 2  || ray->q == 3)
+			if (ray->q == 2 || ray->q == 3)
 				ray->dir = 'W';
 			else
 				ray->dir = 'E';
@@ -349,42 +408,66 @@ void 		ray_dda(t_game *game, t_ray *ray)
 			ray->check = 1;
 	}
 	if (ray->dir == 'W' || ray->dir == 'E')
-		ray->len = (ray->map_pos.x - game->pos.x + (1 - ray->step.x) / 2) / ray->x * 3;
-	else
-		ray->len = (ray->map_pos.y - game->pos.y + (1 - ray->step.y) / 2) / ray->y * 3;
-}
-
-void		ray_draw(t_game *game, t_ray *ray, int i)
-{
-	float start;
-	float end;
-	float len;
-	t_fvector v1;
-	t_fvector v2;
-	len  = 2 * IMG_HEIGHT / ray->len;
-	start = IMG_HEIGHT/2 - ft_min(IMG_HEIGHT / ray->len, IMG_HEIGHT/2 - 1);
-	end = IMG_HEIGHT/2 + ft_min(IMG_HEIGHT / ray->len, IMG_HEIGHT/2 - 1);
-
-	if (ray->dir == 'W')
-		draw_line(game, v1, v2, 0xFF0000);
-	else if (ray->dir == 'E')
-		draw_line(game, v1, v2, 0x0000FF);
-	else if (ray->dir == 'N')
-		draw_line(game, v1, v2, 0xFFFF00);
-	else if (ray->dir == 'S')
-		draw_line(game, v1, v2, 0x00FF00);
-	else
-		draw_line(game, v1, v2, 0xFFFFFF);
-}
-
-void		ray_casting(t_game *game)
-{
-	int		i;
-
-	i = 1;
-	while(i < NUM_RAYS)
 	{
-		t_ray			ray;
+		ray->dis = (ray->map_pos.x - game->pos.x + (1 - ray->step.x) / 2) / ray->x;
+		ray->point = game->pos.y + ray->dis * ray->y;
+	}
+	else
+	{
+		ray->dis = (ray->map_pos.y - game->pos.y + (1 - ray->step.y) / 2) / ray->y;
+		ray->point = game->pos.x + ray->dis * ray->x;
+	}
+}
+
+unsigned int	choice_tex(t_game *game, t_draw draw, char dir)
+{
+	unsigned int color;
+	int			tex_num;
+	if (dir == 'E')
+		tex_num = 0;
+	else if (dir == 'W')
+		tex_num = 1;
+	else if (dir == 'S')
+		tex_num = 2;
+	else if (dir == 'N')
+		tex_num = 3;
+	color = game->tex[tex_num].data[(int)(draw.tex.y * TEXTURE_SIZE) * TEXTURE_SIZE + (int)draw.tex.x];
+	return (color);
+}
+
+void ray_draw(t_game *game, t_ray *ray, int i)
+{
+	t_draw		draw;
+
+	ray->len = IMG_WIDTH / 3 / ray->dis / tan(FOV_ANGLE / 2);
+	draw.pixel = ray->len > IMG_HEIGHT ? (ray->len - IMG_HEIGHT) / 2: 0;
+	draw.step = (ray->len > IMG_HEIGHT) ? (ray->len -  IMG_HEIGHT) / ray->len : 1;
+	draw.start = ft_max((IMG_HEIGHT - ray->len) / 2, 0);
+	draw.end = ft_min((IMG_HEIGHT + ray->len) / 2, IMG_HEIGHT);
+	draw.put.x = i;
+	while (draw.start < draw.end)
+	{
+	draw.tex.x = (ray->point - floor(ray->point)) * 64;
+
+		draw.put.y = draw.start;
+		if (ray->dir == 'W' || ray->dir == 'S')
+			draw.tex.x = TEXTURE_SIZE - draw.tex.x - 1;
+		draw.tex.y = draw.pixel / ray->len;
+		draw.color = choice_tex(game, draw, ray->dir);
+		draw_pixel(game, draw.put, draw.color);
+		draw.start++;
+		draw.pixel++;
+	}
+}
+
+void ray_casting(t_game *game)
+{
+	int i;
+
+	i = 0;
+	while (i < NUM_RAYS)
+	{
+		t_ray ray;
 		ray_init(game, &ray, i);
 		ray_dda_init(game, &ray);
 		ray_dda(game, &ray);
@@ -393,29 +476,27 @@ void		ray_casting(t_game *game)
 	}
 }
 
-int			update_image(t_game *game)
+int update_image(t_game *game)
 {
 	ray_casting(game);
 	{
-	map_draw(game);
-	//player, line
-	for(int i = -5; i < 5; i++)
-		for(int j = -5; j < 5; j++)
-			game->img.data[((int)((game->pos.y * TILESIZE + i)) * IMG_WIDTH + (int)(game->pos.x * TILESIZE + j))] = 0xFF000F;
-	t_fvector v1 = {TILESIZE * game->pos.x + TILESIZE * game->dir.x, TILESIZE * game->pos.y + TILESIZE *game->dir.y};
-	t_fvector v2 = {TILESIZE * game->pos.x, TILESIZE * game->pos.y};
-	draw_line(game, v1, v2, 0xFF00FF);
+		map_draw(game);
+		//player, line
+		for (int i = -5; i < 5; i++)
+			for (int j = -5; j < 5; j++)
+				game->img.data[((int)((game->pos.y * TILESIZE + i)) * IMG_WIDTH + (int)(game->pos.x * TILESIZE + j))] = 0xFF000F;
+		t_fvector v1 = {TILESIZE * game->pos.x + TILESIZE * game->dir.x, TILESIZE * game->pos.y + TILESIZE * game->dir.y};
+		t_fvector v2 = {TILESIZE * game->pos.x, TILESIZE * game->pos.y};
+		draw_line(game, v1, v2, 0xFF00FF);
 	}
-	mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
-	return(0);
+	mlx_put_image_to_window(game->mlx, game->win, game->img.ptr, 0, 0);
+	return (0);
 }
 
-
-
-int		player_move(t_game *game)
+int player_move(t_game *game)
 {
-	float		speed;
-	
+	float speed;
+
 	speed = 0.03;
 	if (game->move.x == 1)
 	{
@@ -436,21 +517,20 @@ int		player_move(t_game *game)
 			game->pos = next_pt;
 		}
 	}
-	return(1);
-
+	return (1);
 }
 
-int		player_turn(t_game *game)
+int player_turn(t_game *game)
 {
 	if (game->turn.x == 1)
 		game->dir = roation(game->dir, M_PI / 120);
 	else if (game->turn.y == 1)
 		game->dir = roation(game->dir, -M_PI / 120);
 	game->plane = roation(game->dir, M_PI / 2);
-	return(1);
+	return (1);
 }
 
-int		main_loop(t_game *game)
+int main_loop(t_game *game)
 {
 	img_init(game);
 
@@ -465,12 +545,11 @@ int		main_loop(t_game *game)
 	return (0);
 }
 
-
-int		main(void)
+int main(void)
 {
-	t_game		game;
+	t_game game;
 	game.pos.x = 6;
-	game.pos.y = 2;
+	game.pos.y = 4;
 	game.dir.x = 1;
 	game.dir.y = 0;
 
