cff7a5ff24e63b360096535f9e303d9be08f6f3d
Hojun Park <hopark@c3r2s4.42seoul.kr>
2021-04-10 20:52:56 +0900

    1

---
+++ 03_libasm/nn/libasm.h
@@ -0,0 +1,50 @@
+#ifndef	LIBASM_H
+# define LIBASM_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdbool.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <errno.h>
+
+# define ESC		"\033"
+# define RED_S		ESC"[31:1m"
+# define GREEN_S	ESC"[32:1m"
+# define YELLOW_S	ESC"[33:1m"
+# define BLUE_S		ESC"[34:1m"
+# define PURPLE_S	ESC"[35:1m"
+# define CYAN_S		ESC"[36:1m"
+# define RESET		ESC"[m"
+
+# define OK			1
+# define KO			0
+# define SUCCESS	0
+# define ERROR		-1
+
+// # define BUFFER_SIZE 512 // write command's max writable size at a time
+# define BUFFER		512 // write command's max writable size at a time
+
+# define ITOC(num) num + '0' 
+
+typedef enum	e_colors{
+	RED_N,
+	GREEN_N,
+	YELLOW_N,
+	BLUE_N,
+	PURPLE_N,	
+	CYAN_N,
+
+	COLORS_NUM
+}				t_colors;
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
+
+#endif
