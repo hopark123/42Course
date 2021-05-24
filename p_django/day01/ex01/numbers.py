93ced6d3abbcaacfa5faef8111dc8e641ca32bb8
Hojun Park <hopark@c10r2s10.42seoul.kr>
2021-05-24 21:55:41 +0900

    1

---
+++ p_django/day01/ex01/numbers.py
@@ -0,0 +1,9 @@
+def number():
+    r = open("numbers.txt", 'r')
+    line = r.readline()
+    for i in line.split(','):
+        print(i.strip())
+    r.close()
+
+if __name__ == '__main__':
+    number()
---
+++ p_django/day01/ex02/var_to_dict.py
@@ -0,0 +1,43 @@
+dicta = dict()
+
+def dictionary(num, year):
+    for i in dicta:
+        if (i == num):
+            print(aaaa)
+            dicta[num].appened(year)
+        else
+            dicta[num] = year
+
+    for j in dcit1:
+        print(j[1] + " : " + j[0])
+        # dict[i[1]] = i[2]
+
+def read_set():
+    for i in d:
+        dictionary(i[1], i[0])
+
+d = [
+('Hendrix' , '1942'),
+('Allman' , '1946'),
+# ('King' , '1925'),
+# ('Clapton' , '1945'),
+# ('Johnson' , '1911'),
+# ('Berry' , '1926'),
+# ('Vaughan' , '1954'),
+# ('Cooder' , '1947'),
+# ('Page' , '1944'),
+# ('Richards' , '1943'),
+# ('Hammett' , '1962'),
+# ('Cobain' , '1967'),
+('Garcia' , '1942'),
+# ('Beck' , '1944'),
+# ('Santana' , '1947'),
+# ('Ramone' , '1948'),
+# ('White' , '1975'),
+# ('Frusciante', '1970'),
+# ('Thompson' , '1949'),
+# ('Burton' , '1939')
+]
+
+if __name__ == '__main__':
+    read_set()
