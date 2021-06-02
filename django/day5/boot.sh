2820b2d7d44697897317877869f38a9010426239
Hojun Park <hopark@c10r2s7.42seoul.kr>
2021-06-02 22:47:43 +0900

    1

---
+++ django/day5/boot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PYTHON_PATH="/usr/bin/python3"
+VEN_DIR="day05_venv"
+
+# setup venv
+$PYTHON_PATH -m venv $VEN_DIR
+source $VEN_DIR/bin/activate
+
+# python version
+python -m pip --version
+
+# pip upgrade
+pip install --upgrade pip
+
+# pip install 
+python -m pip install -q -I django psycopg2-binary
+
+# make requirement.txt
+pip freeze > requirement.txt
+
+# sever start
+# python3 manage.py migrate
+# python3 manage.py runserver
+# django-admin startproject day05
